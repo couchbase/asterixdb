9f27c1404292056cf3659195773d33af52fb1ecc
Hakim Cassimally <hakim.cassimally@couchbase.com>
2023-06-28 15:19:00 +0100

    docs-fun
    Added .gitignore to make sure modules/ isn't checked in

--- asterixdb/asterix-doc/getafix.sh
+++ asterixdb/asterix-doc/getafix.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo DEBUG: running the Collector script
+
 cd $(dirname $0)
 
 rm -rf modules/
 
 mkdir -p modules/asterixdb/partials/
 
-cp -r src/site/markdown/ modules/asterixdb/partials/
+cp -r src/main/markdown/ modules/asterixdb/partials/
 
 
